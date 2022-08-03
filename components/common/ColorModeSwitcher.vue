856a4a5894d864b617efbfe4e158d5890a1c9347
codesinging <codesinging@gmail.com>
2022-08-03 15:20:22 +0800

    新增深色模型切换组件

---
+++ components/common/ColorModeSwitcher.vue
@@ -0,0 +1,27 @@
+<template>
+    <span @click="click" class="cursor-pointer">
+        <component :is="icon"></component>
+    </span>
+</template>
+
+<script setup>
+import {NewComputer, Moon, SunOne} from "@icon-park/vue-next";
+import {computed, ref, watch} from "vue";
+import {useColorMode} from "@vueuse/core";
+
+const mode = useColorMode()
+
+const modes = ['auto', 'light', 'dark']
+const index = ref(0)
+const icons = [NewComputer, SunOne, Moon]
+const icon = computed(() => icons[index.value])
+
+const click = () => {
+    index.value = (index.value >= modes.length - 1) ? 0 : (index.value + 1)
+    mode.value = modes[index.value]
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
     <div>
-        <div class="flex items-center justify-center h-12 bg-gray-200 mt-4 text-2xl text-green-600">Index Page. welcome</div>
-        <div class="mt-4">
-            <new-computer/>
+        <div class="border-b">
+            <color-mode-switcher/>
         </div>
     </div>
 </template>
 
 <script setup>
-import {NewComputer} from "@icon-park/vue-next";
+import ColorModeSwitcher from "../components/common/ColorModeSwitcher.vue";
 </script>
 
 <style scoped>
