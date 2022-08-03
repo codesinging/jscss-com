3347f8be40d895adf14ac1d01679b010ff27462c
codesinging <codesinging@gmail.com>
2022-08-03 10:18:36 +0800

    安装 IconPark 图标库

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="flex items-center justify-center h-12 bg-gray-200 mt-4 text-2xl text-green-600">Index Page. welcome</div>
+    <div>
+        <div class="flex items-center justify-center h-12 bg-gray-200 mt-4 text-2xl text-green-600">Index Page. welcome</div>
+        <div class="mt-4">
+            <new-computer/>
+        </div>
+    </div>
 </template>
 
 <script setup>
-
+import {NewComputer} from "@icon-park/vue-next";
 </script>
 
 <style scoped>
