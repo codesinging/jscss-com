4ced9d58a44f2d8d71f9496992955e6796d6536b
codesinging <codesinging@gmail.com>
2022-08-07 18:05:12 +0800

    新增 PinButton 按钮组件

---
+++ components/icons/PinButton.vue
@@ -0,0 +1,130 @@
+<template>
+    <button
+        :type="nativeType"
+        class="inline-flex items-center justify-center font-medium disabled:cursor-not-allowed focus:outline-none space-x-2"
+        :class="[typeClass, shapeClass, heightClass, widthClass, paddingClass, fontSizeClass]"
+        :disabled="isDisabled"
+        @click="click"
+    >
+        <component :is="icon" :size="iconSize" v-if="icon && !loading"/>
+        <pin-loading-icon v-if="loading" :size="iconSize"/>
+        <span v-if="slots.default" class="inline-flex items-center justify-center"><slot></slot></span>
+    </button>
+</template>
+
+<script setup>
+import {computed, useSlots} from "vue";
+import PinLoadingIcon from "./PinLoadingIcon";
+
+const props = defineProps({
+    nativeType: {
+        type: String,
+        default: 'button',
+    },
+    type: {
+        type: String,
+        default: 'default'
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    loading: {
+        type: Boolean,
+        default: false
+    },
+    shape: {
+        type: String,
+        default: 'default'
+    },
+    size: {
+        type: String,
+        default: 'default'
+    },
+    icon: {
+        type: [String, Object],
+        default: ''
+    },
+})
+
+const typeClass = computed(() => {
+    const classes = {
+        default: 'border border-gray-300 bg-white text-gray-600 hover:bg-gray-50 focus:bg-gray-100 disabled:border-opacity-25 disabled:text-opacity-30 disabled:bg-white',
+        primary: 'border-transparent text-white bg-blue-500 hover:bg-blue-400 focus:bg-blue-600 disabled:text-opacity-50 disabled:bg-blue-300 transition-all',
+        success: 'border-transparent text-white bg-green-500 hover:bg-green-400 focus:bg-green-600 disabled:text-opacity-50 disabled:bg-green-300 transition-all',
+        danger: 'border-transparent text-white bg-red-500 hover:bg-red-400 focus:bg-red-600 disabled:text-opacity-50 disabled:bg-red-300 transition-all',
+    }
+
+    return classes[props.type] || classes.default
+})
+
+const shapeClass = computed(() => {
+    const classes = {
+        default: 'rounded',
+        square: 'rounded-none',
+        rounded: 'rounded-md',
+        pill: 'rounded-full',
+        circle: 'rounded-full overflow-hidden',
+    }
+    return classes[props.shape] || classes.default
+})
+
+const heightClass = computed(() => {
+    const classes = {
+        small: 'h-8',
+        default: 'h-10',
+        large: 'h-12',
+    }
+    return classes[props.size] || classes.default
+})
+
+const widthClass = computed(() => {
+    const classes = {
+        small: 'w-8',
+        default: 'w-10',
+        large: 'w-12',
+    }
+    return props.shape === 'circle'
+        ? (classes[props.size] || classes.default)
+        : null
+})
+
+const paddingClass = computed(() => {
+    const classes = {
+        small: 'px-2',
+        default: 'px-4',
+        large: 'px-12',
+    }
+    return props.shape === 'circle' ? '' : (classes[props.size] || classes.default)
+})
+
+const fontSizeClass = computed(() => {
+    const classes = {
+        small: 'text-xs',
+        default: 'text-base',
+        large: 'text-xl',
+    }
+    return classes[props.size] || classes.default
+})
+
+const iconSize = computed(() => {
+    const sizes = {
+        small: '.75rem',
+        default: '1rem',
+        large: '1.25rem',
+    }
+    return sizes[props.size] || sizes.default
+})
+
+const isDisabled = computed(() => props.disabled || props.loading)
+
+const slots = useSlots()
+
+const emits = defineEmits(['click'])
+
+const click = () => emits('click')
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,52 @@
 <template>
-    <div class="container">
-        {{ site.name }} {{ site.icp }}
+    <div class="container space-y-2">
+        <div>
+            <pin-loading-icon/>
+        </div>
+        <div class="space-x-2">
+            <pin-button>普通按钮</pin-button>
+            <pin-button type="primary">主要按钮</pin-button>
+            <pin-button type="success">危险按钮</pin-button>
+            <pin-button type="danger">危险按钮</pin-button>
+        </div>
+        <div class="space-x-2">
+            <pin-button disabled>普通按钮</pin-button>
+            <pin-button disabled type="primary">主要按钮</pin-button>
+            <pin-button disabled type="success">危险按钮</pin-button>
+            <pin-button disabled type="danger">危险按钮</pin-button>
+        </div>
+        <div class="space-x-2">
+            <pin-button loading>普通按钮</pin-button>
+            <pin-button loading type="primary">主要按钮</pin-button>
+            <pin-button loading type="success">危险按钮</pin-button>
+            <pin-button loading type="danger">危险按钮</pin-button>
+        </div>
+        <div class="space-x-2">
+            <pin-button :icon="Refresh">普通按钮</pin-button>
+            <pin-button type="primary" :icon="Refresh">主要按钮</pin-button>
+            <pin-button type="success" :icon="Refresh">危险按钮</pin-button>
+            <pin-button type="danger" :icon="Refresh">危险按钮</pin-button>
+        </div>
+        <div class="space-x-2">
+            <pin-button shape="circle" :icon="Refresh"></pin-button>
+            <pin-button shape="circle" type="primary" size="small" :icon="Refresh"></pin-button>
+            <pin-button shape="circle" type="success" :icon="Refresh"></pin-button>
+            <pin-button shape="circle" type="danger" size="large" :icon="Refresh"></pin-button>
+        </div>
+        <div class="space-x-2">
+            <pin-button type="primary" size="large">主要按钮</pin-button>
+            <pin-button type="primary">主要按钮</pin-button>
+            <pin-button type="primary" size="small">主要按钮</pin-button>
+        </div>
     </div>
 </template>
 
 <script setup>
 import {useHead} from "nuxt/app";
 import {useSite} from "../composables/useSite";
+import PinLoadingIcon from "../components/icons/PinLoadingIcon";
+import PinButton from "../components/icons/PinButton";
+import {Refresh} from "@icon-park/vue-next";
 
 const site = useSite()
 
