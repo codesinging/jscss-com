3fee905e43bb4e5c38ae96551bc25824e7070314
codesinging <codesinging@gmail.com>
2022-08-10 00:00:53 +0800

    新增带有复制功能的图标组件 PinCopierIcon

---
+++ components/ui/basic/PinCopierIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <component @click="copy(content)" :is="icon" class="cursor-pointer"/>
+</template>
+
+<script setup>
+import {useClipboard} from "@vueuse/core";
+import {Copy, Check} from "@icon-park/vue-next";
+import {computed} from "vue";
+
+defineProps({
+    content: {
+        type: [String, Number, Boolean],
+        default: '',
+    },
+})
+
+const {copy, copied} = useClipboard()
+
+const icon = computed(() => copied.value ? Check : Copy)
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/ui/copier-icon.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container space-y-4 py-4">
+        <div class="space-x-4 flex items-center flex-row">
+            <pin-copier-icon content="content"/>
+            <pin-input v-model="content">
+                <template #suffix>
+                    <pin-copier-icon :content="content"/>
+                </template>
+            </pin-input>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import PinCopierIcon from "../../components/ui/basic/PinCopierIcon";
+import PinInput from "../../components/ui/form/PinInput";
+import {ref} from "vue";
+
+const content = ref('hello')
+</script>
+
+<style scoped>
+
+</style>
