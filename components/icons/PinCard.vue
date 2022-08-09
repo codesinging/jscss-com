10b3ba708f47df3133273ee659417d640452e18b
codesinging <codesinging@gmail.com>
2022-08-09 11:08:09 +0800

    新增 card 组件

---
+++ components/icons/PinCard.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="pin-card" :class="containerClasses">
+        <div v-if="hasHeader" class="pin-card-header">
+            <slot name="header">
+                <div class="pin-card-header-main">
+                    <slot v-if="hasIcon" name="icon">
+                        <component v-if="icon" :is="icon" size="20" class="pin-card-icon"/>
+                    </slot>
+                    <slot v-if="hasTitle" name="title">
+                        <span class="pin-card-title">{{ title }}</span>
+                    </slot>
+                    <slot v-if="hasSubtitle" name="subtitle">
+                        <span class="pin-card-subtitle">{{ subtitle }}</span>
+                    </slot>
+                </div>
+                <div v-if="hasExtra" class="pin-card-header-extra">
+                    <slot name="extra">
+                        <div class="pin-card-header-extra">{{ extra }}</div>
+                    </slot>
+                </div>
+            </slot>
+        </div>
+        <div class="pin-card-content" :class="contentClasses">
+            <slot></slot>
+        </div>
+        <div v-if="hasFooter" class="pin-card-footer">
+            <slot name="footer">{{ footer }}</slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {computed, useSlots} from "vue";
+
+const props = defineProps({
+    title: String,
+    subtitle: String,
+    icon: [String, Object],
+    extra: String,
+    border: Boolean,
+    rounded: Boolean,
+    shadow: Boolean,
+    shadowWhenHover: Boolean,
+    contentPadding: {
+        type: Boolean,
+        default: true
+    },
+    footer: String,
+})
+
+const slots = useSlots()
+
+const hasTitle = computed(() => props.title || slots.title !== undefined)
+const hasSubtitle = computed(() => props.subtitle || slots.subtitle !== undefined)
+const hasIcon = computed(() => props.icon || slots.icon !== undefined)
+const hasExtra = computed(() => props.extra || slots.extra !== undefined)
+
+const hasHeader = computed(() => hasTitle.value || hasSubtitle.value || hasIcon.value || hasExtra.value)
+
+const hasFooter = computed(() => props.footer || slots.footer)
+
+const containerClasses = {
+    border: props.border,
+    rounded: props.rounded,
+    'shadow-lg': props.shadow,
+    'hover:shadow-lg': props.shadowWhenHover,
+}
+
+const contentClasses = {
+    'p-4': props.contentPadding,
+}
+</script>
+
+<style scoped>
+.pin-card {
+    @apply overflow-hidden;
+    @apply bg-white;
+    @apply dark:bg-gray-800;
+}
+
+.pin-card-header {
+    @apply flex items-center justify-between border-b h-14 px-4;
+}
+
+.pin-card-header-main {
+    @apply flex items-center space-x-2;
+}
+
+.pin-card-icon {
+
+}
+
+.pin-card-title {
+    @apply text-base;
+}
+
+.pin-card-subtitle {
+    @apply text-sm text-gray-400;
+}
+
+.pin-card-header-extra {
+    @apply text-sm text-gray-500;
+}
+
+.pin-card-footer {
+    @apply flex items-center justify-between border-t h-12 px-4 text-sm text-gray-500;
+}
+</style>
---
+++ pages/ui/card.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="container space-y-4 py-4">
+        <div class="grid grid-cols-3 gap-4">
+            <pin-card>
+                Content
+            </pin-card>
+            <pin-card border>
+                Content
+            </pin-card>
+            <pin-card border rounded>
+                Content
+            </pin-card>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <pin-card border rounded>
+                Content
+            </pin-card>
+            <pin-card border rounded shadow>
+                Content
+            </pin-card>
+            <pin-card border rounded shadow-when-hover>
+                Content
+            </pin-card>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <pin-card border rounded title="标题">
+                Content
+            </pin-card>
+            <pin-card border rounded subtitle="副标题">
+                Content
+            </pin-card>
+            <pin-card border rounded title="标题" subtitle="副标题">
+                Content
+            </pin-card>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <pin-card border rounded title="标题" :icon="Edit">
+                Content
+            </pin-card>
+            <pin-card border rounded subtitle="副标题" :icon="Edit">
+                Content
+            </pin-card>
+            <pin-card border rounded title="标题" subtitle="副标题" :icon="Edit" extra="这是额外内容">
+                Content
+            </pin-card>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <pin-card title="标题" :icon="Edit">
+                Content
+            </pin-card>
+            <pin-card subtitle="副标题" :icon="Edit">
+                Content
+            </pin-card>
+            <pin-card title="标题" subtitle="副标题" :icon="Edit" extra="这是额外内容">
+                Content
+            </pin-card>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <pin-card border rounded title="标题" :icon="Edit" footer="这里是尾部内容">
+                Content
+            </pin-card>
+            <pin-card border rounded subtitle="副标题" :icon="Edit">
+                Content
+                <template #footer>
+                    <div class="text-gray-500">尾部左侧内容</div>
+                    <pin-button size="small" type="danger">删除</pin-button>
+                </template>
+            </pin-card>
+            <pin-card border rounded title="标题" subtitle="副标题" :icon="Edit" extra="这是额外内容">
+                <template #extra>
+                    <pin-button type="primary" size="small">刷新</pin-button>
+                </template>
+                Content
+                <template #footer>
+                    <div class="flex items-center justify-evenly w-full">
+                        <pin-button size="small" type="primary">修改</pin-button>
+                        <pin-button size="small" type="success">查看</pin-button>
+                        <pin-button size="small" type="danger">删除</pin-button>
+                    </div>
+                </template>
+            </pin-card>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import PinCard from "../../components/icons/PinCard";
+import {Edit} from "@icon-park/vue-next";
+import PinButton from "../../components/icons/PinButton";
+</script>
+
+<style scoped>
+
+</style>
